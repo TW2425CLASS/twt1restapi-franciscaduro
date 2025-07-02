42672f8e95609dfad92ec3de579d6fdf34aec8cf
franciscaduro <franciscaduro@ipvc.pt>
2025-07-02 14:36:39 +0100

    correção server.js

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Ligado ao MongoDB Atlas com sucesso!'))
 .catch((err) => console.error('Erro na ligação:', err));
 
